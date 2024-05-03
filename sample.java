776244fe15353228b7ec523e4a32e63756c38c09
root <root@ip-172-31-45-234.eu-west-2.compute.internal>
2024-05-03 07:19:12 +0000

    This is first commit

---
+++ sample.java
@@ -0,0 +1,16 @@
+import java.util*;
+import java.io.*;
+
+public class sample
+{
+public static void main(String[] args)
+{
+Scanner sc = newScanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int sum=a+b
+System.out.println("Sum="+sum);
+System.out.println("Sum="+sum);
+System.out.println("Sum="+sum);
+}
+}
